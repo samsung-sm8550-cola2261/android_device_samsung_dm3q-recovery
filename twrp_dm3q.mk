b4378626e7cdfa1f8751ceff64439f83ee92a30f
cola2261 <cola2261@proton.me>
2025-12-21 02:15:27 +0500

    dm3q: Introduce OFRP Introduce OrangeFox Recovery Project
    Signed-off-by: cola2261 <cola2261@proton.me>

--- twrp_dm3q.mk
+++ twrp_dm3q.mk
@@ -11,6 +11,9 @@ PRODUCT_RELEASE_NAME := dm3q
 # Inherit device configuration
 $(call inherit-product, device/samsung/$(PRODUCT_RELEASE_NAME)/device.mk)
 
+# Inherit OrangeFox-specific settigns
+$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/$(PRODUCT_RELEASE_NAME)/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,28 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-DEVICE="dm3q"
+FDEVICE="dm3q"
+#set -o xtrace
 
-add_lunch_combo twrp_$DEVICE-eng
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+	lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
